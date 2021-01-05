551b086a3b2b5a4658c83de422a0956faddbdaca
Kirill <aleks.kovalenko.1998@gmail.com>
2021-01-05 15:32:55 +0200

    answer on the second question

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let numberOfFilms = prompt("Сколько фильмов вы смотрели", "");
 
+let prersonalMovieDB = {
+    count : numberOfFilms,
+    movies : {
+
+    },
+    actors : {
+
+
+    },
+    genres : [],
+    privat : false
+};
