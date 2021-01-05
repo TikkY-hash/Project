fce82ff25ec3a88d1dd62ac261bfc4ad25bfa178
Kirill <aleks.kovalenko.1998@gmail.com>
2021-01-05 22:19:50 +0200

    first and second questions

--- script.js
+++ script.js
@@ -3,16 +3,31 @@
 const numberOfFilms = +prompt("Сколько фильмов вы смотрели", "");
 
 const prersonalMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-const firstQuestion = prompt("Один из полседних просмотренных фильмов", ""),
-      secondQuestion = prompt("На сколько оцените его?", "");
 
-prersonalMovieDB.movies[firstQuestion] = secondQuestion;
+
+for (let i = 0; i < 2; i++) {
+    let checkAnswer = true;
+    do {
+        const firstQuestion = prompt("Один из последних просмотренных фильмов", ""),
+            secondQuestion = prompt("На сколько оцените его?", "");
+        if (firstQuestion !== '' && secondQuestion !== '' &&
+            firstQuestion !== null && secondQuestion !== null && firstQuestion.length < 50 && secondQuestion < 50) {
+            prersonalMovieDB.movies[firstQuestion] = secondQuestion;
+            console.log('done');
+        } else {
+            checkAnswer = false;
+            console.log('error');
+        }
+    } while (checkAnswer == false);
+}
+
+
 
 console.log(prersonalMovieDB);
