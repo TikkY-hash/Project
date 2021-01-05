4531e5e5eb409e28393e54fce4d61516ab8adf7e
Kirill <aleks.kovalenko.1998@gmail.com>
2021-01-05 22:29:09 +0200

    correct second answer and done a third question

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const numberOfFilms = +prompt("Сколько фильмов вы смотрели", "");
 
-const prersonalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -10,16 +10,27 @@ const prersonalMovieDB = {
     privat: false
 };
 
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
 
 
 for (let i = 0; i < 2; i++) {
-    let checkAnswer = true;
+    let checkAnswer;
     do {
+        checkAnswer = true;
         const firstQuestion = prompt("Один из последних просмотренных фильмов", ""),
             secondQuestion = prompt("На сколько оцените его?", "");
         if (firstQuestion !== '' && secondQuestion !== '' &&
             firstQuestion !== null && secondQuestion !== null && firstQuestion.length < 50 && secondQuestion < 50) {
-            prersonalMovieDB.movies[firstQuestion] = secondQuestion;
+            personalMovieDB.movies[firstQuestion] = secondQuestion;
             console.log('done');
         } else {
             checkAnswer = false;
@@ -30,4 +41,4 @@ for (let i = 0; i < 2; i++) {
 
 
 
-console.log(prersonalMovieDB);
+console.log(personalMovieDB);
