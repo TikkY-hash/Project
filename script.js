fc7a74a7be7971bc11f74bc85dff635bd89830af
Kirill <aleks.kovalenko.1998@gmail.com>
2021-01-06 15:24:15 +0200

    first task of the third practic

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы смотрели", "");
+let numberOfFilms;
+
+function showNumberOfFilms() {
+    numberOfFilms = prompt("Сколько фильмов вы смотрели", "");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt("Сколько фильмов вы смотрели", "");
+    }
+}
+
+showNumberOfFilms();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,34 +19,48 @@ const personalMovieDB = {
     privat: false
 };
 
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
-}
 
-let i = 0;
+ 
 
-while (i < 2) {
-    let checkAnswer = true;
-    while (checkAnswer == true) {
-        checkAnswer = true;
-        const firstQuestion = prompt("Один из последних просмотренных фильмов", ""),
-            secondQuestion = prompt("На сколько оцените его?", "");
-        if (firstQuestion !== '' && secondQuestion !== '' &&
-            firstQuestion !== null && secondQuestion !== null && firstQuestion.length < 50 && secondQuestion < 50) {
-            personalMovieDB.movies[firstQuestion] = secondQuestion;
-            console.log('done');
-            checkAnswer = false;
-        } else {
-            console.log('error');
-        }
+
+function showCount() {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
     }
-    i++;
 }
+
+showCount();
+
+
+
+
+function sortAnswer() {
+    let i = 0;
+    while (i < 2) {
+        let checkAnswer = true;
+        while (checkAnswer == true) {
+            checkAnswer = true;
+            const firstQuestion = prompt("Один из последних просмотренных фильмов", ""),
+                secondQuestion = prompt("На сколько оцените его?", "");
+            if (firstQuestion !== '' && secondQuestion !== '' &&
+                firstQuestion !== null && secondQuestion !== null && firstQuestion.length < 50 && secondQuestion < 50) {
+                personalMovieDB.movies[firstQuestion] = secondQuestion;
+                console.log('done');
+                checkAnswer = false;
+            } else {
+                console.log('error');
+            }
+        }
+        i++;
+    }
+}
+
+sortAnswer();
 
 console.log(personalMovieDB);
